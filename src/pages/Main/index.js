7646fa99ed88e178ac8cf0d6da262ebe68fcc507
Fbrito1 <luan.tecno@gmail.com>
2019-09-15 13:11:19 -0300

    add styled components

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
+import { Title } from './styles';
+
 export default function Main() {
-  return <h1>Main</h1>;
+  return (
+    <Title error>
+      Main
+      <small>menor</small>
+    </Title>
+  );
 }
