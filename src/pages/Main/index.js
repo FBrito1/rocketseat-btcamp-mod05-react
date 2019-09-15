496e873ed622eb38e1a858e25b7329b9ad917e2a
Fbrito1 <luan.tecno@gmail.com>
2019-09-15 18:53:05 -0300

    styling main page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+
+import { Container, Form, SubmitButton } from './styles';
 
 export default function Main() {
   return (
-    <Title error>
-      Main
-      <small>menor</small>
-    </Title>
+    <Container>
+      <FaGithubAlt />
+      <h1>Repositórios</h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar repositório" />
+
+        <SubmitButton>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
   );
 }
